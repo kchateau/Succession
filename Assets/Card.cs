79415ac3c90ae74cc7b714508d67273218cf2e60
Kendra Chateau <kendrachateau@outlook.com>
2019-08-19 22:13:59 -0700

    Add Desk load
    Add functionality of reading in the csv file of games and creating cards
    for the deck

---
+++ Assets/Card.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card{
+
+    public string name;
+    public string month;
+    public int day;
+    public int year;
+
+}
