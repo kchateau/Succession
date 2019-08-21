cb108e7ea6f1e5983631d4898aa7cca7766442a3
Kendra Chateau <kendrachateau@outlook.com>
2019-08-20 18:01:35 -0700

    Implement deal hand on startup
    Dealhand creates 3 cards from the prefab and deals them into the hand
    panel

--- Assets/DealHand.cs
+++ Assets/DealHand.cs
@@ -1,12 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class CreateDeck {
+public class DealHand : MonoBehaviour{
 
     public List<Card> deck = new List<Card>();
+    public GameObject myPrefab;
+    Text date;
 
-    void Start() {
+    void Start(){
+
+        GenerateDeck();
+
+        for (int i = 0; i < 3; i++) {
+            int x = Random.Range(0, 850);
+
+            var panel = GameObject.Find("Hand");
+
+            if (panel != null) {
+                GameObject a = (GameObject)Instantiate(myPrefab);
+                a.transform.SetParent(panel.transform, false);
+                Debug.Log(deck[x].name);
+            }
+        }     
+    }
+
+    private void GenerateDeck() {
         TextAsset gameData = Resources.Load<TextAsset>("game_data");
 
         string[] data = gameData.text.Split(new char[] { '\n' });
@@ -23,7 +43,8 @@ public class CreateDeck {
                 int.TryParse(row[3], out c.year);
 
                 deck.Add(c);
-            }                    
+            }
         }
     }
+
 }
