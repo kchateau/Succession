1a601dff9fd83207894f9b1a3c154de09bbbdd19
Kendra Chateau <kendrachateau@outlook.com>
2019-12-20 09:42:44 -0800

    Create restart button on win scene
    You can now restart the game when you win.

---
+++ Assets/RestartGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RestartGame : MonoBehaviour{
+
+    private Button restartButton;
+
+    void Awake(){
+        restartButton = GetComponent<Button>();
+    }
+
+    void Start(){
+        restartButton = GameObject.Find("restart").GetComponent<Button>();
+        restartButton.onClick.AddListener(OnRestartClick);
+    }
+
+    public void OnRestartClick(){
+        SceneManager.LoadScene("main");
+    }
+}
